28cf3f3ff9c003112e05571a57d603d7e6e51bec
mozoroff <mozoshima@gmail.com>
2022-02-22 14:29:42 +0900

    init

---
+++ src/Deck.java
@@ -0,0 +1,31 @@
+
+public class Deck {
+	private String suit;
+	private String rank;
+	
+	public void getRankNmber() {
+		
+	}
+	
+	
+	public Deck(String suit, String rank) {
+		super();
+		this.suit = suit;
+		this.rank = rank;
+	}
+	
+	public String getSuit() {
+		return suit;
+	}
+	public void setSuit(String suit) {
+		this.suit = suit;
+	}
+	public String getRank() {
+		return rank;
+	}
+	public void setRank(String rank) {
+		this.rank = rank;
+	}
+
+
+}
